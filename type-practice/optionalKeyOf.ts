9faf63127e33278da24b1006e2ba0d233d9e2cf1
github xxbbxx825 <xxbbxx825@gmail.com>
2022-05-11 22:13:54 +0900

    add: optionalKeyOf Type

---
+++ type-practice/optionalKeyOf.ts
@@ -0,0 +1,19 @@
+type Data = {
+  foo: string
+  bar?: number
+  baz?: boolean
+  hoge: undefined
+  piyo?: undefined
+}
+
+type OptionalKeyOf<T extends object> = {
+  [K in keyof T]: T extends Record<K, T[K]> ? never : K
+}[keyof T]
+
+type Sample1 = Data extends Record<'foo', Data['foo']> ? never : 'aiye'
+type Sample2 = Record<'foo', Data['foo']>
+type Sample3 = Data extends Record<'bar', Data['bar']> ? never : 'aiye'
+type Sample4 = Record<'bar', Data['bar']>
+
+// "bar" | "baz" | "piyo"
+type OptionalKeyOfData = OptionalKeyOf<Data>
